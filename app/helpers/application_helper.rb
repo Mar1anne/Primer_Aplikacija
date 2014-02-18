547e9ddbbe28b29eed22d36befec89241046a469
Mar1anne <mariana.ristovska@gmail.com>
2014-02-18 21:49:32 +0100

    Added title helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Vrakja cel naslov ako naslovot na stranicata e prazen
+  
+  def full_title(page_title)
+    base_title = "Social App"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,12 +9,16 @@ describe "Static pages" do
       expect(page).to have_content('Social App')
     end
 
-    it "should have the title 'Home'" do
+   it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Social App | Home")
+      expect(page).to have_title("Social App")
     end
-  end
 
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
+end
   describe "Help page" do
 
     it "should have the content 'Help'" do
